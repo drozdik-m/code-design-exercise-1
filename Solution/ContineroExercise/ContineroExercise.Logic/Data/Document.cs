89f6cf32c2789a157969a94d926ad5afb613af54
Martin Drozdík <drozdik.m@seznam.cz>
2022-07-17 10:36:25 +0200

    Add document data model

---
+++ Solution/ContineroExercise/ContineroExercise.Logic/Data/Document.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContineroExercise.Logic.Data
+{
+    public class Document
+    {
+        public string Title { get; set; } = string.Empty;
+
+        public string Text { get; set; } = string.Empty;
+    }
+}
